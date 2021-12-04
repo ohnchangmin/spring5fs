c3a5e48afeb120164a1f03ef7c791cb2f838329b
ohnchangmin <ok_ddol@naver.com>
2021-12-04 23:30:00 +0900

    프린터 예제 빈 설정파일에 등록

--- sp5-chap03/src/main/java/config/AppCtx.java
+++ sp5-chap03/src/main/java/config/AppCtx.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 import spring.ChangePasswordService;
 import spring.MemberDao;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
 import spring.MemberRegisterService;
 
 @Configuration
@@ -25,5 +27,15 @@ public class AppCtx {
 		ChangePasswordService pwdSvc = new ChangePasswordService();
 		pwdSvc.setMemberDao(memberDao());
 		return pwdSvc;
+	}
+	
+	@Bean
+	public MemberPrinter memberPrinter() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	public MemberListPrinter listPrinter() {
+		return new MemberListPrinter(memberDao(), memberPrinter());
 	}
 }
